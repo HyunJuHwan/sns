bb668ae0bb644dece21124a6db2c2d6460ce16e7
HyunJuHwan <guswnghks0703@gmail.com>
2023-12-20 11:33:13 +0900

    localLogin update

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(morgan('dev')); // logging dev = dev -> combined = prod
 app.use(express.static(path.join(__dirname, 'public'))); //public dir static으로 변경, __dirname = SNS 경로와 public 경로를 join하고 static으로 변경
 app.use(express.json()); // json요청 받게
 app.use(express.urlencoded({extended:false})); //form 요청 받게
-app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(cookieParser(process.env.COOKIE_SECRET)); //{connect.sid : 랜덤쿠키값} 객체로 만들어줌
 app.use(session( {
     resave : false,
     saveUninitialized : false,
@@ -50,7 +50,7 @@ app.use(session( {
     }
 }))
 app.use(passport.initialize()); // 꼭 express.session 미들웨어 밑에 작성해야한다. , req.user, req.login, req.isAuthenticated, req.logout 함수 사용 가능
-app.use(passport.session()); // connect.sid라는 이름으로 세션 쿠키가 브라우저로 전송
+app.use(passport.session()); // connect.sid라는 이름으로 세션 쿠키가 브라우저로 전송, 로그인 이후 요청이 들어오면 deserializeUser 메서드 호출
 
 app.use('/', pageRouter);
 app.use('/auth', authRouter);
@@ -63,7 +63,7 @@ app.use((err, req, res, next) =>{ //error처리 미들웨어
     res.locals.message = err.message;
     res.locals.error = process.env.NODE_ENV !== 'production' ? err : {}; //에러 로그를 서비스한테 넘긴다. 현재 표시 x
     res.status(err.status || 500);
-    req.render('error');
+    res.render('error');
 });
 
 app.listen(app.get('port'), () => {
--- routes/page.js
+++ routes/page.js
@@ -4,7 +4,7 @@ const { renderProfile, renderJoin, renderMain } = require('../controllers/page')
 const {isLoggedIn, isNotLoggedIn} = require('../middlewares');
 
 router.use((req, res, next) => { 
-    res.locals.user = req.user; //router에서 공통으로 사용하는 변수
+    res.locals.user = req.user; //router(미들웨어)에서 공통으로 사용하는 변수 , req.session은 사용자 별 공유 데이터(같은 사용자는 로그아웃 전까지 여기에 데이터가 저장된다.)
     res.locals.followerCount = 0;
     res.locals.followingCount = 0;
     res.locals.followerIdList = [];
